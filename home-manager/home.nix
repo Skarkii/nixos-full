b4def59a88c3f41822682fcb697f9b67b14e7073
Skarkii <lucas@kuja-halkola.com>
2025-05-11 05:48:03 +0200

    default browser

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -67,6 +67,18 @@
     # '';
   };
 
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "librewolf.desktop";
+      "x-scheme-handler/http" = "librewolf.desktop";
+      "x-scheme-handler/https" = "librewolf.desktop";
+      "x-scheme-handler/about" = "librewolf.desktop";
+      "x-scheme-handler/unknown" = "librewolf.desktop";
+      "application/xhtml+xml" = "librewolf.desktop";
+    };
+  };
+
   home.sessionVariables = {
   };
 
